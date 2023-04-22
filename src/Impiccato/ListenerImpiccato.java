8a3a1680f76ac7e3dbd9b7b02cdba79a2c62dd74
Ciollo <93654785+Ciollo@users.noreply.github.com>
2023-04-22 11:21:35 +0200

    Update ListenerImpiccato.java
    sistemato pulzia codice

--- src/Impiccato/ListenerImpiccato.java
+++ src/Impiccato/ListenerImpiccato.java
@@ -5,6 +5,13 @@ LUOGO: CASA
 OGGETTO: CLASSE LISTENER IMPICCATO
 */
 
+/** TODO
+ *  - Sistemare switch case del mostra punteggio, case 0 significa immagine 0, 
+ * non e un errore ma è per avere un senso logico 
+ *  - controllare se l'array di char puo essere sostituito in qualche modo dalle stringhe
+ * 
+*/
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,7 +20,7 @@ import java.util.Vector;
 public class ListenerImpiccato implements ActionListener {
 
     //impiccato
-    Impiccato impiccato;
+    private Impiccato impiccato;
 
     //metodo costruttore della classe ListenerImpiccato
     public ListenerImpiccato(Impiccato impiccato){
@@ -24,47 +31,47 @@ public class ListenerImpiccato implements ActionListener {
     private void mostraPersonaggio(){
         switch (impiccato.getTentativi()){
             case 0:
-                impiccato.lblImmagine.setIcon(impiccato.images[6]);
+                impiccato.getLblImmagine().setIcon(impiccato.getImages()[6]);
                 break;
             case 1:
-                impiccato.lblImmagine.setIcon(impiccato.images[5]);
+                impiccato.getLblImmagine().setIcon(impiccato.getImages()[5]);
                 break;
             case 2:
-                impiccato.lblImmagine.setIcon(impiccato.images[4]);
+                impiccato.getLblImmagine().setIcon(impiccato.getImages()[4]);
                 break;
             case 3:
-                impiccato.lblImmagine.setIcon(impiccato.images[3]);
+                impiccato.getLblImmagine().setIcon(impiccato.getImages()[3]);
                 break;
             case 4:
-                impiccato.lblImmagine.setIcon(impiccato.images[2]);
+                impiccato.getLblImmagine().setIcon(impiccato.getImages()[2]);
                 break;
             case 5:
-                impiccato.lblImmagine.setIcon(impiccato.images[1]);
+                impiccato.getLblImmagine().setIcon(impiccato.getImages()[1]);
                 break;
             case 6:
-                impiccato.lblImmagine.setIcon(impiccato.images[0]);
+                impiccato.getLblImmagine().setIcon(impiccato.getImages()[0]);
                 break;
         }
     }
 
     //metodo che restituisce true se la lettera è presente altrimenti restituisce false
     private boolean letteraPresente(){
-        char[] lettera = impiccato.cslParolaUtente.getText().toCharArray();
-        char[] parola = impiccato.lblParolaNascosta.getText().toCharArray();
-        boolean trovato = false;
-        for(int i=1; i<impiccato.lblParolaNascosta.getText().length()-1; i++){
+        char[] lettera = impiccato.getCslParolaUtente().getText().toCharArray();
+        char[] parola = impiccato.getLblParolaNascosta().getText().toCharArray();
+        boolean istrovato = false;
+        for(int i=1; i<impiccato.getLblParolaNascosta().getText().length()-1; i++){
             if(lettera[0] == impiccato.getParolaEstratta().charAt(i)){
                 parola[i] = impiccato.getParolaEstratta().charAt(i);
-                impiccato.lblParolaNascosta.setText(new String(parola));
-                trovato = true;
+                impiccato.getLblParolaNascosta().setText(new String(parola));
+                istrovato = true;
             }
         }
-        return trovato;
+        return istrovato;
     }
 
     //metodo che restituisce true se la lettera è stata già detta altrimenti restituisce false
     private boolean letteraGiaDetta(){
-        char[] lettera = impiccato.cslParolaUtente.getText().toCharArray();
+        char[] lettera = impiccato.getCslParolaUtente().getText().toCharArray();
         for (int i = 0; i < impiccato.getLettereGiaDette().size(); i++){
             if(impiccato.getLettereGiaDette().get(i) == lettera[0]){
                 return true;
@@ -75,44 +82,44 @@ public class ListenerImpiccato implements ActionListener {
 
     //metodo per indovinare una lettera nella parola
     private void indovinaLettera(){
-        boolean letteraGiaDetta = letteraGiaDetta();
-        boolean letteraPresente = letteraPresente();
-        boolean parolaNonIndovinata = true;
-        if(!letteraGiaDetta && letteraPresente) {
-            if(impiccato.getParolaEstratta().equals(impiccato.lblParolaNascosta.getText())){
-                impiccato.lblIndovinato.setText("Hai indovinato!");
-                impiccato.lblIndovinato.setForeground(Color.green);
-                impiccato.lblParolaNascosta.setText(impiccato.getParolaEstratta());
-                impiccato.cslParolaUtente.setEditable(false);
-                impiccato.giocoFinito = true;
-                parolaNonIndovinata = false;
+        boolean isLetteraGiaLetta = letteraGiaDetta();
+        boolean isLetteraPresente = letteraPresente();
+        boolean isParolaNonIndovinata = true;
+        if(!isLetteraGiaLetta && isLetteraPresente) {
+            if(impiccato.getParolaEstratta().equals(impiccato.getLblParolaNascosta().getText())){
+                impiccato.getLblIndovinato().setText("Hai indovinato!");
+                impiccato.getLblIndovinato().setForeground(Color.green);
+                impiccato.getLblParolaNascosta().setText(impiccato.getParolaEstratta());
+                impiccato.getCslParolaUtente().setEditable(false);
+                impiccato.finisciGioco();
+                isParolaNonIndovinata = false;
             } else {
-                impiccato.lblIndovinato.setText("Lettera " + impiccato.cslParolaUtente.getText() + " presente nella parola!");
-                impiccato.lblIndovinato.setForeground(Color.green);
+                impiccato.getLblIndovinato().setText("Lettera " + impiccato.getCslParolaUtente().getText() + " presente nella parola!");
+                impiccato.getLblIndovinato().setForeground(Color.green);
             }
-        } else if (letteraGiaDetta) {
+        } else if (isLetteraGiaLetta) {
             impiccato.setTentativi(impiccato.getTentativi()-1);
-            impiccato.lblIndovinato.setText("Lettera " + impiccato.cslParolaUtente.getText() + " già detta! \nTentativi rimasti: " + impiccato.getTentativi());
-            impiccato.lblIndovinato.setForeground(Color.red);
+            impiccato.getLblIndovinato().setText("Lettera " + impiccato.getCslParolaUtente().getText() + " già detta! \nTentativi rimasti: " + impiccato.getTentativi());
+            impiccato.getLblIndovinato().setForeground(Color.red);
             mostraPersonaggio();
         } else {
             impiccato.setTentativi(impiccato.getTentativi()-1);
-            impiccato.lblIndovinato.setText("Lettera " + impiccato.cslParolaUtente.getText() + " non presente! \nTentativi rimasti: " + impiccato.getTentativi());
-            impiccato.lblIndovinato.setForeground(Color.red);
+            impiccato.getLblIndovinato().setText("Lettera " + impiccato.getCslParolaUtente().getText() + " non presente! \nTentativi rimasti: " + impiccato.getTentativi());
+            impiccato.getLblIndovinato().setForeground(Color.red);
             mostraPersonaggio();
         }
 
         Vector<Character> lettereGiaDette = impiccato.getLettereGiaDette();
-        lettereGiaDette.add(impiccato.cslParolaUtente.getText().toCharArray()[0]);
+        lettereGiaDette.add(impiccato.getCslParolaUtente().getText().toCharArray()[0]);
         impiccato.setLettereGiaDette(lettereGiaDette);
-        if(parolaNonIndovinata){
-            impiccato.cslParolaUtente.setText("");
+        if(isParolaNonIndovinata){
+            impiccato.getCslParolaUtente().setText("");
         }
     }
 
     //metodo che restituisce true se la parola è stata già detta altrimenti restituisce false
     private boolean parolaGiaDetta(){
-        String parola = impiccato.cslParolaUtente.getText();
+        String parola = impiccato.getCslParolaUtente().getText();
         for (int i = 0; i < impiccato.getParoleGiaDette().size(); i++){
             if(impiccato.getParoleGiaDette().get(i).equals(parola)){
                 return true;
@@ -123,48 +130,52 @@ public class ListenerImpiccato implements ActionListener {
 
     //metodo per indovinare la parola direttamente
     private void indovinaParola(){
-        boolean parolaGiaDetta = parolaGiaDetta();
-        boolean parolaNonIndovinata = true;
-        if (!parolaGiaDetta && impiccato.getParolaEstratta().equals(impiccato.cslParolaUtente.getText())){
-            impiccato.lblIndovinato.setText("Hai indovinato!");
-            impiccato.lblIndovinato.setForeground(Color.green);
-            impiccato.lblParolaNascosta.setText(impiccato.getParolaEstratta());
-            impiccato.cslParolaUtente.setEditable(false);
-            parolaNonIndovinata = false;
-            impiccato.giocoFinito = true;
-        } else if (parolaGiaDetta){
+        boolean isParolaGiaDetta = parolaGiaDetta();
+        boolean isParolaNonIndovinata = true;
+        if (!isParolaGiaDetta && impiccato.getParolaEstratta().equals(impiccato.getCslParolaUtente().getText())){
+            impiccato.getLblIndovinato().setText("Hai indovinato!");
+            impiccato.getLblIndovinato().setForeground(Color.green);
+            impiccato.getLblParolaNascosta().setText(impiccato.getParolaEstratta());
+            impiccato.getCslParolaUtente().setEditable(false);
+            isParolaNonIndovinata = false;
+            impiccato.finisciGioco();
+        } else if (isParolaGiaDetta){
             impiccato.setTentativi(impiccato.getTentativi()-1);
-            impiccato.lblIndovinato.setText("Parola " + impiccato.cslParolaUtente.getText() + " già detta! \nTentativi rimasti: " + impiccato.getTentativi());
-            impiccato.lblIndovinato.setForeground(Color.red);
+            impiccato.getLblIndovinato().setText("Parola " + impiccato.getCslParolaUtente().getText() + " già detta! \nTentativi rimasti: " + impiccato.getTentativi());
+            impiccato.getLblIndovinato().setForeground(Color.red);
             mostraPersonaggio();
         } else {
             impiccato.setTentativi(impiccato.getTentativi()-1);
-            impiccato.lblIndovinato.setText("Parola " + impiccato.cslParolaUtente.getText() + " non corretta! \nTentativi rimasti: " + impiccato.getTentativi());
-            impiccato.lblIndovinato.setForeground(Color.red);
+            impiccato.getLblIndovinato().setText("Parola " + impiccato.getCslParolaUtente().getText() + " non corretta! \nTentativi rimasti: " + impiccato.getTentativi());
+            impiccato.getLblIndovinato().setForeground(Color.red);
             mostraPersonaggio();
         }
         Vector<String> paroleGiaDette = impiccato.getParoleGiaDette();
-        paroleGiaDette.add(impiccato.cslParolaUtente.getText());
+        paroleGiaDette.add(impiccato.getCslParolaUtente().getText());
         impiccato.setParoleGiaDette(paroleGiaDette);
-        if(parolaNonIndovinata){
-            impiccato.cslParolaUtente.setText("");
+        if(isParolaNonIndovinata){
+            impiccato.getCslParolaUtente().setText("");
         }
     }
 
     //metodo actionPerformed
     public void actionPerformed(ActionEvent e){
-        if(!impiccato.giocoFinito){
-            if(impiccato.cslParolaUtente.getText().length() == 1) indovinaLettera();
-            else indovinaParola();
+        if(!impiccato.getIsGiocoFinito()){
+            if(impiccato.getCslParolaUtente().getText().length() == 1) {
+                indovinaLettera();
+            } else {
+                indovinaParola();
+            }
+
             if(impiccato.getTentativi() == 0){
-                impiccato.lblIndovinato.setText("GAME OVER!");
-                impiccato.lblIndovinato.setForeground(Color.red);
-                impiccato.lblParolaNascosta.setText(impiccato.getParolaEstratta());
-                impiccato.giocoFinito = true;
+                impiccato.getLblIndovinato().setText("GAME OVER!");
+                impiccato.getLblIndovinato().setForeground(Color.red);
+                impiccato.getLblParolaNascosta().setText(impiccato.getParolaEstratta());
+                impiccato.finisciGioco();
             }
         } else {
-            impiccato.lblIndovinato.setText("GIOCO CONCLUSO!");
-            impiccato.lblIndovinato.setForeground(Color.BLACK);
+            impiccato.getLblIndovinato().setText("GIOCO CONCLUSO!");
+            impiccato.getLblIndovinato().setForeground(Color.BLACK);
         }
     }
 }
