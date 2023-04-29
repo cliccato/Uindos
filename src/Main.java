53aa91397e1113747528354df735ce03d139de1a
Ciollo <93654785+Ciollo@users.noreply.github.com>
2023-04-29 21:06:43 +0200

    Delete src directory

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-import System.Login.LoginFrame;
-
-public class Main {
-    public static void main(String[] args) {
-        new LoginFrame("admin");//login di default come admin, nella macchina virtuale login come altro utente
-    }
-}
