4d01443d47ab107cb78b62fb3fb85589c80cff8d
Ciollo <93654785+Ciollo@users.noreply.github.com>
2023-04-29 21:09:44 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import System.Login.LoginFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        new LoginFrame();//login di default come admin, nella macchina virtuale login come altro utente
+    }
+}
---
+++ src/utils/App.java
@@ -0,0 +1,10 @@
+package utils;
+public class App {
+    private String name;
+    private String logoPath;
+
+    public App(String name, String logoPath) {
+        this.name = name;
+        this.logoPath = logoPath;
+    }
+}
