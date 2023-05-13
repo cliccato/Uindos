7ed92318b4bf46cf1ba01d557233e2c4bf36ca72
Ciollo <giorgiofasullo68@gmail.com>
2023-05-13 22:41:37 +0200

    fixed start

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import System.Desktop.DesktopFrame;
 import System.Login.LoginFrame;
-import app.Paint.PaintApp;
 
 public class Main {
     public static void main(String[] args) {
-        // new LoginFrame();
-        new PaintApp();
+        new LoginFrame();
+        // new PaintApp();
     }
 }
