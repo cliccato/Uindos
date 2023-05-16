c747983306e66ec8566fec9a9905d86394495aa9
Studente <studente@didattica.local>
2023-05-16 09:19:57 +0200

    start fix path

---
+++ src/utils/UindosPath.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class UindosPath {
+    public static final String BROWSER_LOGO_PATH = "images/logo/browser-logo.png";
+    public static final String CALCULATOR_LOGO_PATH = "images/logo/calculator-logo.png";
+}
